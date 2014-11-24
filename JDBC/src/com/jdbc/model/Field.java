33f2d0bbff957fc3d8bfa7cfde584d0b6f8bcb37
Administrator <Administrator@ZeroXia>
2014-11-24 17:30:40 +0800

    add

--- JDBC/src/com/jdbc/model/Field.java
+++ JDBC/src/com/jdbc/model/Field.java
@@ -4,20 +4,51 @@ import java.sql.Types;
 
 /**
  * Table Field
- * @author Administrator
+ * @author Administrator 
  * 2014年11月24日
  */
 public class Field<E> {
-	
-	private String fieldName;		//field name
-	private E fieldValue;			//field value
-	private Types type;				//field type
+
+	private String fieldName; // field name
+	private E fieldValue; // field value
+	private int type; // field type
+
+	public Field(String fieldName) {
+		this(fieldName, null);
+	}
 
 	public Field(String fieldName, E fieldValue) {
-		super();
+		this(fieldName, fieldValue, Types.VARCHAR);
+	}
+
+	public Field(String fieldName, E fieldValue, int type) {
 		this.fieldName = fieldName;
 		this.fieldValue = fieldValue;
+		this.type = type;
 	}
-	
-	
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public E getFieldValue() {
+		return fieldValue;
+	}
+
+	public void setFieldValue(E fieldValue) {
+		this.fieldValue = fieldValue;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
 }
--- JDBC/src/com/jdbc/model/SQLStatement.java
+++ JDBC/src/com/jdbc/model/SQLStatement.java
@@ -1,5 +1,8 @@
 package com.jdbc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * SQL statement
  * @author Administrator
@@ -8,8 +11,31 @@ package com.jdbc.model;
 public class SQLStatement {
 	
 	private final String WHERE = " WHERE ";
-	private OperateType opType;			//DML type
-	private String tableName;			//table name;
+	private OperateType opType;					//DML type
+	private String tableName;					//table name;
+	private List<Table> tables;					//table
+	
+	private StringBuffer sqlBuff;				//SQL statement 
+	
+	private SQLStatement(){
+		super();
+		tables = new ArrayList<Table>();
+		sqlBuff = new StringBuffer();
+	}
+	
+	public SQLStatement(OperateType opType){
+		this();
+		this.opType = opType;
+	}
+	
+	public void addTable(Table table){
+		this.tables.add(table);
+	}
+	
+	public void setTable(Table table){
+		this.tables.clear();
+		this.tables.add(table);
+	}
 	
 	
 }
