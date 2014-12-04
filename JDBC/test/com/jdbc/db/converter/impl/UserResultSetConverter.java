1098361e5d6b03a072ec5a060e4f36c8af600045
unknown <Administrator@.(none)>
2014-12-04 17:27:16 +0800

    JUnit Test

--- JDBC/test/com/jdbc/db/converter/impl/UserResultSetConverter.java
+++ JDBC/test/com/jdbc/db/converter/impl/UserResultSetConverter.java
@@ -14,9 +14,9 @@ public class UserResultSetConverter implements IResultSetConverter<User> {
 			return null;
 		}
 		User user = new User();
-		user.setId(rs.getLong("user_id"));
-		user.setUsername(rs.getString("user_name"));
-		user.setPassword(rs.getString("user_password"));
+		user.setId(rs.getLong("id"));
+		user.setUsername(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
 		return user;
 	}
 
--- JDBC/test/com/jdbc/db/helper/DBManagerTest.java
+++ JDBC/test/com/jdbc/db/helper/DBManagerTest.java
@@ -8,13 +8,6 @@ import java.util.List;
 
 import org.junit.Test;
 
-
-
-
-
-
-
-
 import com.jdbc.db.converter.ConverterFactory;
 import com.jdbc.db.converter.IResultSetConverter;
 import com.jdbc.db.converter.impl.UserResultSetConverter;
@@ -23,30 +16,55 @@ import com.jdbc.test.bean.User;
 public class DBManagerTest {
 
 	@Test
-	public void test() {
-		List<User> us = DBManager.queryToList("select * from t_user where user_id=?",new Object[]{1}, new UserResultSetConverter());
-		for(User u : us){
+	public void testList() {
+		List<User> us = DBManager.queryToList(
+				"select * from user", 
+				null,
+				new UserResultSetConverter());
+		for (User u : us) {
 			System.out.println(u);
 		}
-		
-		String password = DBManager.queryToBean("select user_password from t_user where user_id = ?",
-				new Object[]{1}, 
-				new IResultSetConverter<String>(){
+	}
+
+	@Test
+	public void testBean() {
+		String password = DBManager.queryToBean(
+				"select password from user where id = ?",
+				new Object[] { 10 }, 
+				new IResultSetConverter<String>() {
 					@Override
 					public String conver(ResultSet rs) throws SQLException {
 						String password = null;
-						while(rs.next()){
+						while (rs.next()) {
 							password = rs.getString(1);
 						}
 						return password;
 					}
 				});
 		System.out.println(password);
-		
-		String password2 = DBManager.queryToBean("select user_password from t_user where user_id = ?",
-				new Object[]{1}, 
-				ConverterFactory.getConverter(String.class));
-		System.out.println(password2);
 	}
 
+	@Test
+	public void testSingleColumn() {
+		String password = DBManager
+				.queryToBean(
+						"select password from user where id = ?",
+						new Object[] { 1 },
+						ConverterFactory.getConverter(String.class));
+		System.out.println(password);
+	}
+
+	@Test
+	public void testInsert(){
+		int flag = DBManager.executeUpdate(
+				"INSERT INTO USER VALUES(?,?,?)", 
+				new Object[]{20, "张三", "12121212"});
+		System.out.println(flag);
+	}
+	
+	@Test
+	public void testDelete(){
+		int flag = DBManager.executeUpdate("delete from user where id=20", null);
+		System.out.println(flag);
+	}
 }
